f5bbf56bf7ec241df507ced8148a5aab1ea59a05
Eugene Kharitonov <eugene-kharitonov@ya.ru>
2016-08-02 00:09:48 +0200

    fix

--- src/models/json_util.h
+++ src/models/json_util.h
@@ -3,7 +3,7 @@
 #include <Eigen/Dense>
 
 namespace OctopiJsonHelper {
-    void ParseVector(Eigen::MatrixXd &v, const picojson::value &o) {
+    inline void ParseVector(Eigen::MatrixXd &v, const picojson::value &o) {
         const picojson::value::array &b = o.get<picojson::value::array>();
         for(int j = 0; j < b.size(); ++j) {
             double d = b[j].get<double>();
@@ -11,7 +11,7 @@ namespace OctopiJsonHelper {
         };
     };
 
-    static void ParseMatrix(Eigen::MatrixXd &m, const picojson::value &o) {
+    inline static void ParseMatrix(Eigen::MatrixXd &m, const picojson::value &o) {
         const picojson::value::array &b = o.get<picojson::value::array>();
         for(int i = 0; i < b.size(); ++i) {
             const picojson::value::array &row = b[i].get<picojson::value::array>();
